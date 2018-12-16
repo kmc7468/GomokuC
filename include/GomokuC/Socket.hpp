1b4c8001383013987d27239d925451a14629702e
static <kmc7468@naver.com>
2018-12-16 22:14:25 +0900

    Socket 클래스 추가
    이 클래스는 WinSock2의 SOCKET 타입의 래퍼입니다.

---
+++ include/GomokuC/Socket.hpp
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include <WinSock2.h>
+
+enum class AddressFamily
+{
+	IPv4 = AF_INET,
+	IPv6 = AF_INET6,
+};
+
+class Socket final
+{
+public:
+	Socket();
+	explicit Socket(AddressFamily af);
+	Socket(const Socket& socket) = delete;
+	Socket(Socket&& socket) noexcept;
+	~Socket();
+
+public:
+	Socket& operator=(const Socket& socket) = delete;
+	Socket& operator=(Socket&& socket) noexcept;
+	bool operator==(const Socket& socket) = delete;
+	bool operator!=(const Socket& socket) = delete;
+
+public:
+	void disconnect() noexcept;
+
+public:
+	SOCKET data() noexcept;
+
+private:
+	SOCKET socket_ = INVALID_SOCKET;
+};
---
+++ src/Socket.cpp
@@ -0,0 +1,43 @@
+﻿#include <GomokuC/Socket.hpp>
+
+#include <stdexcept>
+
+Socket::Socket()
+	: Socket(AddressFamily::IPv4)
+{}
+Socket::Socket(AddressFamily af)
+	: socket_(socket(static_cast<int>(af), SOCK_STREAM, IPPROTO_TCP))
+{
+	if (socket_ == INVALID_SOCKET)
+		throw std::runtime_error("Failed to create a socket.");
+}
+Socket::Socket(Socket&& socket) noexcept
+	: socket_(socket.socket_)
+{
+	socket.socket_ = INVALID_SOCKET;
+}
+Socket::~Socket()
+{
+	disconnect();
+}
+
+Socket& Socket::operator=(Socket&& socket) noexcept
+{
+	socket_ = socket.socket_;
+	socket.socket_ = INVALID_SOCKET;
+
+	return *this;
+}
+
+void Socket::disconnect() noexcept
+{
+	if (socket_ != INVALID_SOCKET)
+	{
+		closesocket(socket_);
+	}
+}
+
+SOCKET Socket::data() noexcept
+{
+	return socket_;
+}
